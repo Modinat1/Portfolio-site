a2bb55456a29a98c13b219e955816c38761d4307
Modinat1 <adenikem@yahoo.com>
2022-07-02 09:28:59 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const burger = document.getElementById('burger');
+const navList = document.querySelector('.navlist');
+
+// hamburger toggler event
+burger.addEventListener('click', () =>{
+    navList.classList.toggle('show');
+})
