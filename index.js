e704a4a4eb7c6119967f0eebb569139b9c4c5079
Modinat1 <adenikem@yahoo.com>
2022-09-23 07:45:25 +0100

    smooth scrolling added

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ const navList = document.querySelector('.navlist');
 burger.addEventListener('click', () =>{
     navList.classList.toggle('show');
 })
+
+// Smooth Scrolling
+$('#navbar a').on('click', function (event){
+    if(this.hash !=- ''){
+        event.preventDefault();
+        const hash = this.hash;
+        $('html, body').animate(
+           {
+            scrollTop: $(hash).offset().top - 20
+           },
+           800
+        );
+    }
+});
